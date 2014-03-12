39c947369d43f0b3159cd5284f99bf2b8c560808
Bryan Deeney <bdeeney@pobox.com>
2014-03-12 11:16:20 -0400

    Return JSON responses

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-from flask import abort, Flask, request
+from flask import abort, Flask, jsonify, request, url_for
 
 from sharded_counter import (GeneralCounterShardConfig as Counter, get_count,
                              increment)
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def root():
+    """Return a link to the counters collection."""
+    return jsonify(counters=url_for('create_counter', _external=True))
+
 
 @app.route('/counters/', methods=['POST'])
 def create_counter():
     """Create a new counter and return its id and URL."""
     counter_id = str(Counter().put().integer_id())
-    return str({'id': counter_id}), 201
+    url = url_for('get_counter', counter_id=counter_id, _external=True)
+    response = jsonify(id=counter_id, url=url)
+    response.status_code = 201
+    response.headers.add('Location', url)
+    return response
 
 
 @app.route('/counters/<int:counter_id>', methods=['GET', 'PUT'])
 def get_counter(counter_id):
+    """Get or increment the current value of a counter."""
     if Counter.get_by_id(counter_id) is None:
         abort(404)
     counter_id = str(counter_id)
@@ -22,4 +32,4 @@ def get_counter(counter_id):
     if request.method == 'PUT':
         increment(counter_id)
 
-    return str({'count': get_count(counter_id)})
+    return jsonify(id=counter_id, url=request.url, count=get_count(counter_id))
